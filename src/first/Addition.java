89371b7f9984ca824ad0e671ac57049ad07af147
Admin <Admin@DESKTOP-P6KQ8GT>
2022-04-24 09:57:18 +0530

    First Commit

---
+++ src/first/Addition.java
@@ -0,0 +1,14 @@
+package first;
+
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  int a=2;
+  int b=4;
+  int c=a+b;
+ System.out.println(c);
+	}
+
+}
