83764cbeabb8492ee1d78cb2d75bea6bec324aee
Admin <Admin@DESKTOP-P6KQ8GT>
2022-05-27 11:04:44 +0530

    first commit

--- src/exception/Exception9.java
+++ src/exception/Exception9.java
@@ -4,19 +4,14 @@ public class Exception9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-try
-{
-int data=100/0;
-System.out.println(data);
-}
-catch (Exception e)
-{
-	System.out.println("can't devide integer by 0");
-}
-finally
-{
-	System.out.println("The 'try catch'is finished. ");
-}
+		try {
+			int data = 100 / 0;
+			System.out.println(data);
+		} catch (Exception e) {
+			System.out.println("can't devide integer by 0");
+		} finally {
+			System.out.println("The 'try catch'is finished. ");
+		}
 	}
 
 }
